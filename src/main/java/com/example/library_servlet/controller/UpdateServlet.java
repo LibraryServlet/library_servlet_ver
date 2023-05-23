cbda69125444f4a70e2c65a0b9cc04a4daeab67a
nanajin <nmj5182@gmail.com>
2023-05-23 21:39:43 +0900

    DESIGN: 수정 부분 css 추가

--- src/main/java/com/example/library_servlet/controller/UpdateServlet.java
+++ src/main/java/com/example/library_servlet/controller/UpdateServlet.java
@@ -35,12 +35,24 @@ public class UpdateServlet extends HttpServlet {
         pw.println("height: 100vh;");
         pw.println("background-color: #f5f5f5;");
         pw.println("}");
+        pw.println("#regbox {\n" +
+                "            width: 400px;\n" + "height: 100vh;"+
+                "            background-color: #fff;\n" +
+                "            padding: 20px;\n" +
+                "            border: 1px solid #ccc;\n" +
+                "            border-radius: 5px;\n" +
+                "            margin: auto;\n" +
+                "            margin-top: 10%; \n" +
+                "        }");
+        pw.println("#regbox textarea{" +
+                "resize: none;" +
+                "width: 90%}");
         pw.println("label {");
         pw.println("  display: block;");
         pw.println("  margin-top: 10px;");
         pw.println("}");
-        pw.println("input[type='text'], textarea {");
-        pw.println("  width: 600px;");
+        pw.println("#regbox input[type='text'], textarea {");
+        pw.println("  width: 90%;");
         pw.println("  padding: 5px;");
         pw.println("  border-radius: 4px;");
         pw.println("  border: 1px solid #ccc;");
@@ -50,7 +62,9 @@ public class UpdateServlet extends HttpServlet {
         pw.println("}");
         pw.println("h1 { text-align: center; }");
         pw.println("input[type='submit'] {");
-        pw.println("  margin-top: 20px;");
+        pw.println("  margin-bottom: 50px;");
+        pw.println("  margin-top: 50px;");
+        pw.println("  margin-left: 150px;");
         pw.println("  padding: 8px 12px;");
         pw.println("  font-size: 16px;");
         pw.println("  background-color: #4CAF50;");
@@ -85,8 +99,8 @@ public class UpdateServlet extends HttpServlet {
 
 
         pw.println("<form method='post' action='updateok'>");
+        pw.println("<div id=\"regbox\">");
         pw.println("<h1>도서 수정</h1>");
-
         pw.println("<input type='hidden' name='id' value='" + id + "'>");
         pw.println("<label for='name'>책 이름</label>");
         pw.println("<input type='text' name='name' value='" + lib.getName() + "'><br/>");
