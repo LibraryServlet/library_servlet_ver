b1d5afb3a4a111e685c41507b92c8da912fab731
oddnine <dnwnsgur12@gmail.com>
2023-05-23 18:08:27 +0900

    -

--- src/main/java/com/example/library_servlet/controller/UpdateServlet.java
+++ src/main/java/com/example/library_servlet/controller/UpdateServlet.java
@@ -25,30 +25,67 @@ public class UpdateServlet extends HttpServlet {
         resp.setContentType("text/html;charset=euc-kr");
         PrintWriter pw = resp.getWriter();
         pw.println("<html>");
-        pw.println("<head></head>");
+        pw.println("<head>");
+        pw.println("<title>도서 수정</title>");
+        pw.println("<style>");
+        pw.println("body {");
+        pw.println("display: flex;");
+        pw.println("justify-content: center;");
+        pw.println("align-items: center;");
+        pw.println("height: 100vh;");
+        pw.println("background-color: #f5f5f5;");
+        pw.println("}");
+        pw.println("label {");
+        pw.println("  display: block;");
+        pw.println("  margin-top: 10px;");
+        pw.println("}");
+        pw.println("input[type='text'], textarea {");
+        pw.println("  width: 600px;");
+        pw.println("  padding: 5px;");
+        pw.println("  border-radius: 4px;");
+        pw.println("  border: 1px solid #ccc;");
+        pw.println("}");
+        pw.println("textarea {");
+        pw.println("  height: 150px;");
+        pw.println("}");
+        pw.println("h1 { text-align: center; }");
+        pw.println("input[type='submit'] {");
+        pw.println("  margin-top: 20px;");
+        pw.println("  padding: 8px 12px;");
+        pw.println("  font-size: 16px;");
+        pw.println("  background-color: #4CAF50;");
+        pw.println("  border: none;");
+        pw.println("  border-radius: 4px;");
+        pw.println("  color: white;");
+        pw.println("}");
+        pw.println("</style>");
+        pw.println("</head>");
         pw.println("<body>");
 
+
         pw.println("<form method='post' action='updateok'>");
+        pw.println("<h1>도서 수정</h1>");
+
         pw.println("<input type='hidden' name='id' value='" + id + "'>");
         pw.println("<label for='name'>책 이름</label>");
-        pw.println("<input type='text' name='name' value= " + lib.getName() + "><br/>");
+        pw.println("<input type='text' name='name' value='" + lib.getName() + "'><br/>");
         pw.println("<label for='author'>작가</label>");
-        pw.println("<input type='text' name='author' value=" + lib.getAuthor() + "><br/>");
+        pw.println("<input type='text' name='author' value='" + lib.getAuthor() + "'><br/>");
         pw.println("<label for='publisher'>출판사</label>");
-        pw.println("<input type='text' name='publisher' value=" + lib.getPublisher() + "><br/>");
+        pw.println("<input type='text' name='publisher' value='" + lib.getPublisher() + "'><br/>");
         pw.println("<label for='isbn'>ISBN</label>");
-        pw.println("<input type='text' name='isbn' value=" + lib.getIsbn() + "><br/>");
+        pw.println("<input type='text' name='isbn' value='" + lib.getIsbn() + "'><br/>");
         pw.println("<label for='releaseYear'>출판 연도</label>");
-        pw.println("<input type='text' name='releaseYear' value=" + lib.getReleaseYear() + "><br/>");
+        pw.println("<input type='text' name='releaseYear' value='" + lib.getReleaseYear() + "'><br/>");
 
         pw.println("<label for='count'>수량</label>");
-        pw.println("<input type='text' name='count' value=" + lib.getCount() + "><br/>");
+        pw.println("<input type='text' name='count' value='" + lib.getCount() + "'><br/>");
         pw.println("<label for='summary'>요약</label>");
-        pw.println("<input type='text' name='summary' value=" + lib.getSummary() + "><br/>");
+        pw.println("<textarea name='summary'>" + lib.getSummary() + "</textarea><br/>");
         pw.println("<label for='image'>이미지</label>");
-        pw.println("<input type='text' name='image' value=" + lib.getImage() + "><br/>");
+        pw.println("<input type='text' name='image' value='" + lib.getImage() + "'><br/>");
         pw.println("<label for='category'>카테고리</label>");
-        pw.println("<input type='text' name='category' value=" + lib.getCategory() + "/><br/>");
+        pw.println("<input type='text' name='category' value='" + lib.getCategory() + "'><br/>");
 
         pw.println("<input type='submit' value='수정 완료'/><br/>");
         pw.println("</form>");
@@ -56,6 +93,5 @@ public class UpdateServlet extends HttpServlet {
         pw.println("</body>");
         pw.println("</html>");
         pw.close();
-
     }
 }
